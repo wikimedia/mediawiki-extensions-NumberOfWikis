aa02b00e3574972205ca07554963e2e06d1981ad
Jack Phoenix <ashley@users.mediawiki.org>
2011-05-10 14:23:45 +0000

    more ShoutWiki goodies

---
+++ NumberOfWikis.i18n.magic.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Internationalization file for NumberOfWikis extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$magicWords = array();
+
+/** English (English) */
+$magicWords['en'] = array(
+	'numberofwikis' => array( 0, 'NUMBEROFWIKIS' ),
+);
+
+/** Finnish (Suomi) */
+$magicWords['fi'] = array(
+	'numberofwikis' => array( 0, 'WIKIENLUKUMÄÄRÄ' ),
+);
+
+/** French (Français) */
+$magicWords['fr'] = array(
+	'numberofwikis' => array( 0, 'NOMBREWIKIS' ),
+);
+
+/** Dutch (Nederlands) */
+$magicWords['nl'] = array(
+	'numberofwikis' => array( 0, 'AANTALWIKIS' ),
+);
+
+/** Polish (Polski) */
+$magicWords['pl'] = array(
+	'numberofwikis' => array( 0, 'LICZBAWIKI' ),
+);
+
+/** Portuguese (Português) */
+$magicWords['pt'] = array(
+	'numberofwikis' => array( 0, 'NUMERODEWIKIS' ),
+);
+
+/** Swedish (Svenska) */
+$magicWords['sv'] = array(
+	'numberofwikis' => array( 0, 'ANTALWIKIS' ),
+);
---
+++ NumberOfWikis.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Number of wikis -- a magic word to show the number of wikis on ShoutWiki
+ *
+ * @file
+ * @ingroup Extensions
+ * @version 0.1
+ * @date November 6, 2010
+ * @author Jack Phoenix <jack@shoutwiki.com>
+ * @license http://en.wikipedia.org/wiki/Public_domain Public domain
+ */
+
+if( !defined( 'MEDIAWIKI' ) ) {
+	die( "This is not a valid entry point.\n" );
+}
+
+// Extension credits that will show up on Special:Version
+$wgExtensionCredits['variable'][] = array(
+	'name' => 'Number of wikis',
+	'version' => '0.1',
+	'author' => 'Jack Phoenix',
+	'description' => 'Adds <nowiki>{{NUMBEROFWIKIS}}</nowiki> magic word to show the number of wikis on ShoutWiki',
+);
+
+// Translations for {{NUMBEROFWIKIS}}
+$dir = dirname( __FILE__ ) . '/';
+$wgExtensionMessagesFiles['NumberOfWikis'] = $dir . 'NumberOfWikis.i18n.magic.php';
+
+$wgHooks['LanguageGetMagic'][] = 'wfNumberOfWikisMagicWord';
+function wfNumberOfWikisMagicWord( &$magicWords, $langID ) {
+	// tell MediaWiki that {{NUMBEROFWIKIS}} and all case variants found in
+	// wiki text should be mapped to magic ID 'NUMBEROFWIKIS'
+	// (0 means case-insensitive)
+	$magicWords['NUMBEROFWIKIS'] = array( 0, 'NUMBEROFWIKIS' );
+	return true;
+}
+
+$wgHooks['ParserGetVariableValueSwitch'][] = 'wfNumberOfWikisAssignValue';
+function wfNumberOfWikisAssignValue( &$parser, &$cache, &$magicWordId, &$ret ) {
+	global $wgMemc;
+
+	if ( $magicWordId == 'NUMBEROFWIKIS' ) {
+		$key = wfMemcKey( 'shoutwiki', 'numberofwikis' );
+		$data = $wgMemc->get( $key );
+		if ( $data != '' ) {
+			// We have it in cache? Oh goody, let's just use the cached value!
+			wfDebugLog(
+				'NumberOfWikis',
+				'Got the amount of wikis from memcached'
+			);
+			// return value
+			$ret = $data;
+		} else {
+			// Not cached → have to fetch it from the database
+			global $wgSharedDB;
+			$dbr = wfGetDB( DB_SLAVE, array(), $wgSharedDB );
+			$res = $dbr->select(
+				'wiki_list',
+				'COUNT(*) AS count',
+				array(),
+				__METHOD__
+			);
+			wfDebugLog( 'NumberOfWikis', 'Got the amount of wikis from DB' );
+			foreach( $res as $row ) {
+				// Store the count in cache...
+				// (86400 = seconds in a day)
+				$wgMemc->set( $key, $row->count, 86400 );
+				// ...and return the value to the user
+				$ret = $row->count;
+			}
+		}
+	}
+	return true;
+}
+
+$wgHooks['MagicWordwgVariableIDs'][] = 'wfNumberOfWikisVariableIds';
+function wfNumberOfWikisVariableIds( &$variableIds ) {
+	$variableIds[] = 'NUMBEROFWIKIS';
+	return true;
+}
